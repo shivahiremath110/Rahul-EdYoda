ce109c8b2c89eafb911159bd28d3901967ad6a79
rahul Kumar <rahulk2081994@gmail.com>
2025-03-09 06:14:07 +0000

    Updated infra.ps1

--- infra/infra.ps1
+++ infra/infra.ps1
@@ -4,3 +4,5 @@ parm {
 }
 
 New-AzResourceGroup -Name $name -Location $name
+
+New-AzVm -ResourceGroupName "myResourceGroupVM" -Name "$name" -Location "EastUS" -VirtualNetworkName "myVnet" -SubnetName "mySubnet" -SecurityGroupName "myNetworkSecurityGroup" -PublicIpAddressName "myPublicIpAddress" -Credential $cred

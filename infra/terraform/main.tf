43de71d0afd52ee5ea201eaf7d90d4a40dd2aae7
rahul Kumar <rahulk2081994@gmail.com>
2025-04-27 06:07:47 +0000

    Updated main.tf

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -37,7 +37,6 @@ terraform {
 resource "azurerm_virtual_network" "vnet" {
   name                = "Demo-virtual-network"
   location            = azurerm_resource_group.rg.location
-  #location            = "eastus"
   resource_group_name = azurerm_resource_group.rg.name
   address_space       = ["10.0.0.0/16"]
 }
